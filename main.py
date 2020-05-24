83b7f963828a9df09d3652aae0463483b216aaeb
bootlegbilly <ninja25538@tutanota.de>
2020-05-24 14:09:49 -0400

    Beeps for each player between specified values Now for each player between the specified values (in this case 1.05 and 1.1), it makes a beeping sound. However, it will only beep for a player one time

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from bet365 import *
+from playsound import playsound
 import time
 import sys
 import os
@@ -9,24 +10,30 @@ def removekey(d, key):
     del r[key]
     return r
 
+playersNotToBeepFor = []
 
 while True:
     os.system('cls')
-    data = get_odds_data("Table Tennis", False)
+    data = get_odds_data("Table Tennis", True)
     newData = data
     foundInBetween = False
+
 
     if data is not None:
         #Removes players with the string SP
         for players in data:
             if data[players] == 'SP':
-                print(data)
                 newData = removekey(data, players)
 
         for players in newData:
             if 1.05 <= float(newData[players]) <= 1.1:
                 print(players + ": " + newData[players])
                 foundInBetween = True
+                # Thank you to qubodup
+                # https://freesound.org/people/qubodup/sounds/443026/
+                if players not in playersNotToBeepFor:
+                    playsound("beep.wav")
+                    playersNotToBeepFor.append(players)
 
         if not foundInBetween:
             print("No players have odds between 1.05 and 1.1, waiting 30 seconds...")
