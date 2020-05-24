5e45837e6a66aa215e549d0e5ce362867dc823a0
bootlegbilly <ninja25538@tutanota.de>
2020-05-24 15:37:03 -0400

    'SP' Bug Fix Instead of messing around with making a new array without the 'SP' in it, the code just skips over any player with SP as their value

--- main.py
+++ main.py
@@ -15,19 +15,15 @@ playersNotToBeepFor = []
 while True:
     os.system('cls')
     data = get_odds_data("Table Tennis", True)
-    newData = data
     foundInBetween = False
 
 
     if data is not None:
         #Removes players with the string SP
-        for players in data:
-            if data[players] == 'SP':
-                newData = removekey(data, players)
 
-        for players in newData:
-            if 1.05 <= float(newData[players]) <= 1.1:
-                print(players + ": " + newData[players])
+        for players in data:
+            if data[players] != 'SP' and 1.05 <= float(data[players]) <= 1.1:
+                print(players + ": " + data[players])
                 foundInBetween = True
                 # Thank you to qubodup
                 # https://freesound.org/people/qubodup/sounds/443026/
@@ -38,7 +34,6 @@ while True:
         if not foundInBetween:
             print("No players have odds between 1.05 and 1.1, waiting 30 seconds...")
 
-        data = newData
         time.sleep(30)
 
 
